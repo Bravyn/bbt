f6327dfc2c09927686fa2872edbd262149681945
Ian Bravyn <ianbravynsa@gmail.com>
2024-01-29 17:23:39 +0300

    Update bbt.py

--- bbt.py
+++ bbt.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
 def bbt():
-    st.info("**Date: March 29, 2005**")
+    family = ["Martin Bromiley(Husband)", "Elaine(Wife)", "Victoria(Daughter)", "Adam(Son)"]
+    st.info("**March 29, 2005.**")
     st.caption("6:15 am [raining]")
     st.write("A family of four prepares for a surgery. ")
     with st.expander("Family details"):
-        st.info("")
+        for family_member in family:
+            st.info(family_member)
 
     st.success("Reason for surgery: **Sinus Problems.**")
     st.info("Estimated risk level: **Tiny.**")
