6215372264b266bba72731480f62005d7ee1bdb1
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-24 15:32:08 +0300

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,18 @@
+import random
+
+def number_guessing_game():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess the number (Hint: It is between 0 - 100)"))
+        attempts += 1
+        if guess < number:
+            print("Too low, try again")
+        elif guess > number:
+            print("Too high, try again")
+        else:
+            print(f"Congratulations! Guessed right in {attempts} attempts")
+            break
+
+number_guessing_game()
